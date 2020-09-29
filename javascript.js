2c8ac560edd5ceb191c3137e603faec3e75e5333
zoher cheddad <arthasdarky@gmail.com>
2020-09-29 05:53:40 +0200

    la suite

--- javascript.js
+++ javascript.js
@@ -1,3 +1,28 @@
 let maVar = "  les étudiants";
 console.log("Bonjour" + maVar);
-console log("Comment allez-vous?")
+console.log("Comment allez-vous?");
+//commentaire une ligne
+let theGame = "begin";
+/*
+*
+* Commentaire
+* sur plusieurs lignes
+*/
+let anotherVariable ="be";
+
+/*declaration d'une variable*/
+let maVariable;
+maVariable = 42;//assigantion
+
+let maVar  = 101; //le tout d'un coup à préferer
+let maVar = 100;
+
+
+const PI = 3.14159265359;
+const PI = 20;
+
+var answer = 42;
+answer = "Yolo";// pas faire ce type de changement de typage
+
+let x = 'the answer is + 42';
+let y = 42 + ' is the answer';
